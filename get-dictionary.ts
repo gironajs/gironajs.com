8a06c910e0c67bba943f35abfd1398f2630650fb
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2023-03-07 16:28:55 +0100

    fix: Solve eslint and TS issues

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -1,13 +1,15 @@
-import 'server-only'
-import type { Locale } from './i18n-config'
+import 'server-only';
+import type { Locale } from './i18n-config';
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the defalt import for cleaner types
 const dictionaries = {
   ca: () => import('./dictionaries/ca.json').then((module) => module.default),
   en: () => import('./dictionaries/en.json').then((module) => module.default),
-}
+};
+
+export type LangDictionary = Awaited<ReturnType<typeof dictionaries[Locale]>>;
 
 export const getDictionary = async (locale: Locale) => {
-  return dictionaries[locale]()
-}
+  return dictionaries[locale]();
+};
