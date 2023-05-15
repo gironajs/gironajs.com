e98ddf8aad8fc33d9c9700daa2dc54cb2e898ea6
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2023-05-15 16:32:09 +0200

    chore: Empty members array

--- config/member.ts
+++ config/member.ts
@@ -7,47 +7,6 @@ export type Member = {
   company?: string;
 };
 
-// TODO! Empty list
-const members: Member[] = [
-  {
-    name: 'Sergio Gómez',
-    github: 'sergiogc9',
-    role: 'Software Engineer',
-    company: 'Typeform',
-    linkedin: 'sergiogc9',
-  },
-  {
-    name: 'Llorenç Pujol',
-    github: 'llorenspujol',
-    website: 'http://gironafc.com',
-  },
-  {
-    name: 'Sergio Gómez',
-    github: 'sergiogc9',
-    role: 'Software Engineer',
-    company: 'Typeform',
-    website: 'http://gironafc.com',
-  },
-  {
-    name: 'Llorenç Pujol',
-    github: 'llorenspujol',
-    role: 'Software Engineer',
-    company: 'Typeform',
-    website: 'http://gironafc.com',
-  },
-  {
-    name: 'Llorenç Pujol',
-    github: 'llorenspujol',
-    role: 'Software Engineer',
-    company: 'Typeform',
-    website: 'http://gironafc.com',
-  },
-  {
-    name: 'Sergio Gómez',
-    github: 'sergiogc9',
-    role: 'Software Engineer',
-    website: 'http://gironafc.com',
-  },
-];
+const members: Member[] = [];
 
 export { members };
